ea82aafe5c25a6e9a9e8e287548501678525dbab
Vhal999Vhal999 <107368993+Vhal999Vhal999@users.noreply.github.com>
2025-03-28 09:47:51 +0530

    Update play.py

--- PURVIMUSIC/utils/inline/play.py
+++ PURVIMUSIC/utils/inline/play.py
@@ -90,10 +90,10 @@ def stream_markup(_, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="𝖮𝗐𝗇𝖾𝗋🥀", url="https://t.me/THC_OWNER_SAHIL",
+             #   text="𝖮𝗐𝗇𝖾𝗋🥀", url="https://t.me/THC_OWNER_SAHIL",
             ),
             InlineKeyboardButton(
-                text="𝖲𝗎𝗉𝗉𝗈𝗋𝗍🥀", url="https://t.me/ONLY_PUBLIC_LINK_SR",
+               # text="𝖲𝗎𝗉𝗉𝗈𝗋𝗍🥀", url="https://t.me/ONLY_PUBLIC_LINK_SR",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
